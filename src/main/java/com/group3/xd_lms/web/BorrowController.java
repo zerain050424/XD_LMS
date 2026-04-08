d006a2712686fd57d4718fe247a6aec5ca30dfb3
qihanshi <qhsstone2005@163.com>
2026-04-08 21:26:07 +0800

    Reader: 实现 borrowBook 接口，扫描RFID码进行借书

--- src/main/java/com/group3/xd_lms/web/BorrowController.java
+++ src/main/java/com/group3/xd_lms/web/BorrowController.java
@@ -1,12 +1,17 @@
 package com.group3.xd_lms.web;
+import com.group3.xd_lms.entity.BookItem;
 import com.group3.xd_lms.entity.BorrowRecord;
+import com.group3.xd_lms.entity.User;
+import com.group3.xd_lms.mapper.BookItemMapper;
 import com.group3.xd_lms.mapper.BorrowRecordMapper;
+import com.group3.xd_lms.mapper.UserMapper;
 import com.group3.xd_lms.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -16,9 +21,13 @@ import java.util.Map;
 public class BorrowController {
     // 注入Mapper
     private final BorrowRecordMapper borrowRecordMapper;
+    private final BookItemMapper bookItemMapper;
+    private final UserMapper userMapper;
 
-    public BorrowController(BorrowRecordMapper borrowRecordMapper) {
+    public BorrowController(BorrowRecordMapper borrowRecordMapper, BookItemMapper bookItemMapper, UserMapper userMapper) {
         this.borrowRecordMapper = borrowRecordMapper;
+        this.bookItemMapper = bookItemMapper;
+        this.userMapper = userMapper;
     }
     // ==========================================
     // 1. Reader:
@@ -31,8 +40,30 @@ public class BorrowController {
     // TODO R1(Reader) 接收RFID码和用户信息，进行借书
     @RequestMapping(value = {"/borrowBook", "/reader/borrowBook"})
     @Transactional
-    public void borrowBook(){
-       //扫描RFID码进行借书
+    public Map<String, Object> borrowBook(@RequestParam String rfidTag,@RequestParam Long userId){
+        //扫描RFID码进行借书
+
+        BookItem bookItem = bookItemMapper.selectByRfidTag(rfidTag);
+        User user = userMapper.selectById(userId);
+        if(bookItem == null){ // 未找到书目
+            return Result.getResultMap(500,"查询书籍失败");
+        }
+        if(user == null){
+            return Result.getResultMap(500,"查询用户失败");
+        }
+        if(bookItem.isAvailable()){
+            BorrowRecord borrowRecord = new BorrowRecord();
+            borrowRecord.setUserId(userId);
+            borrowRecord.setRfidTag(rfidTag);
+            borrowRecord.setBorrowDate(LocalDateTime.now());
+            borrowRecord.setDueDate(LocalDateTime.now().plusDays(40));
+            borrowRecord.setUser(user);
+            bookItem.setStatus(BookItem.BookStatus.Loaned);
+            borrowRecordMapper.insert(borrowRecord);
+            return Result.getResultMap(200,"借阅成功");
+        }else {
+            return Result.getResultMap(200,"书籍不可借，借阅失败");
+        }
     }
 
     // 还书
